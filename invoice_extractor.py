dbfa4c3cf5e2dbd138e58ab52f7af2b52b675938
chiragmansuriya <168201682+chiragmansuriya@users.noreply.github.com>
2024-11-03 23:05:44 +0530

    Update invoice_extractor.py

--- invoice_extractor.py
+++ invoice_extractor.py
@@ -1,11 +1,7 @@
 # End to End Multi Language Invoice Extractor Project using Gemini Pro LLM Model
 
 from dotenv import load_dotenv
-from langchain_core.utils.image import image_to_data_url
-from streamlit import file_uploader
-
 load_dotenv() # load all the environment variables from .env
-
 import streamlit as st
 import os
 from PIL import Image
@@ -14,9 +10,8 @@ import google.generativeai as genai
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
 # Function to load Gemini Pro Vision
-
 model = genai.GenerativeModel('gemini-1.5-pro')
-#model = genai.GenerativeModel('gemini-1.5-flash')
+
 def get_gemini_response(input, image, prompt):
     response = model.generate_content([input, image[0], prompt])
     return response.text
@@ -35,25 +30,24 @@ def input_image_details(uploaded_file):
         ]
         return image_parts
     else:
-        raise FileNotFoundError("No file uploaded")
-# Initialize our streamlit app
+        raise FileNotFoundError("No file uploaded ")
 
+# Initialize our streamlit app
 st.set_page_config(page_title="MultiLanguage Invoice Extractor")
 st.header("Gemini Application")
-input = st.text_input("Input Prompt: ", key="Input")
 uploaded_file = st.file_uploader("Choose an image of invoice", type=["jpg", "jpeg", "png"])
 image=""
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
+input = st.text_input("Input Prompt: ", key="Input")
 submit = st.button("Tell me about the invoice")
-
 input_prompt = '''
 Your are an expert in understanding invoices. We will upload a image as invoice
 and you will have to answer any questions based on the uploaded invoice image
 '''
-
 
 # if submit button is clicked
 if submit:
